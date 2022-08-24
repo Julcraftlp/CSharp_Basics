6bc31ccee7c0b0b2de9487059f6d675a6f9ff87c
julia <julia@DESKTOP-B6229QJ>
2022-08-24 14:22:23 +0200

    Projektdateien hinzufügen.

---
+++ TestFrame/Program.cs
@@ -0,0 +1,19 @@
+﻿string name;
+string alter;
+string klasse;
+
+Console.Write("Bitte Vorname eingeben:");
+name = Console.ReadLine();
+Console.Write("\nBitte Alter eingeben:");
+alter = Console.ReadLine();
+Console.Write("\nBitte Klassen-Name eingeben:");
+klasse = Console.ReadLine();
+
+Console.WriteLine("\n\n{0} ist {1} Jahre alt und " +
+    "besucht die {2} des Georg-Simon-Ohm",name,alter,klasse);
+
+name = "Juian";
+alter = "15";
+String jn = "nein";
+Console.WriteLine("{0} | {1} | Ist C# meine erste programmiersprche? {2}",
+    name,alter,jn);
