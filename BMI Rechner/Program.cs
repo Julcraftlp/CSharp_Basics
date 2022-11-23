dab6cbfcec288d8bce27a62d5982edef0021236d
Julian Pankratz <julcrafter01@gmail.com>
2022-11-23 12:08:20 +0100

    bmi und prämien

---
+++ BMI Rechner/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Eingabe der Körpergröße in cm bitte:");
+double L = Convert.ToInt32(Console.ReadLine());
+L = L / 100;
+Console.Write("Eingabe des Gewichts bitte:");
+int GW = Convert.ToInt32(Console.ReadLine());
+double bmi = GW / (L * L);
+if (bmi < 18.5)
+    Console.WriteLine("Untergewicht");
+else if (bmi > 25)
+    Console.WriteLine("Übergewicht");
+else
+    Console.WriteLine("Normalgewicht");
---
+++ Treueprämien/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.Write("Bitte anzahl an jahren eingeben:");
+int J = Convert.ToInt32(Console.ReadLine());
+long X = 0;
+while (J >= 3)
+{
+    
+    if (J < 10)
+        X = X + 150;
+    else
+    {
+        int I = J;
+        I = I - 9;
+        I = I * 10;
+        I = I + 150;
+        X = X + I;
+    }
+    --J;
+}
+Console.WriteLine("{0}€ Prämie",X);
