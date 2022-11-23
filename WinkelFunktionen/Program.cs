a0c3c30a7a71308aaf9df08dd7733933ba29cdc7
Julian Pankratz <julcrafter01@gmail.com>
2022-11-23 12:36:34 +0100

    WinkerlFunktionen

---
+++ WinkelFunktionen/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("WinkelRechner");
+Console.WriteLine("1.Cosinus");
+Console.WriteLine("2.Sinus");
+Console.WriteLine("3.Tangenz");
+Console.Write("Bitte Wählen sie:");
+short programm = Convert.ToInt16(Console.ReadLine());
+Console.Write("Bitte Winkel in ° eingeben:");
+double winkel = Convert.ToDouble(Console.ReadLine());
+switch (programm)
+{
+    case 1:
+        Console.WriteLine(Math.Cos(winkel));
+        break;
+    case 2:
+        Console.WriteLine(Math.Sin(winkel));
+        break;
+    case 3:
+        Console.WriteLine(Math.Tan(winkel));
+        break;
+    default:
+        Console.WriteLine("ERROR #404\nFalsche Eingabe");
+        break;
+}
